9c3d0a65e02d08abce9abadbadb71b9eb2d637b0
RodrigoRogerSilva <rrds1802@gmail.com>
2024-11-30 13:05:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleText(id) {
+    const element = document.getElementById(id);
+    if (element.classList.contains('hidden')) {
+        element.classList.remove('hidden');
+    } else {
+        element.classList.add('hidden');
+    }
+}
